18f2b8bc0e2b3af3ed970ad75891b70fb1272eee
Роман <roman.v.antonnikov@gmail.com>
2025-12-26 14:02:40 +0300

    Вернул задержку

--- bot.py
+++ bot.py
@@ -2545,7 +2545,7 @@ async def main():
         seed_data(sess, anxiety_codes=None)
         sess.commit()
 
-    await asyncio.sleep(10)
+    await asyncio.sleep(15)
     asyncio.create_task(check_all_shipped_orders())
 
     while True:
